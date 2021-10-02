3187e93635c8ea6a37b022c59d9f294f248cb719
Mada Satya Bayu Ambika <madasatya6@gmail.com>
2021-10-02 18:51:27 +0700

    update mux

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/madasatya6/go-native
+
+go 1.17
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
     "fmt"
+	"net/http"
+	"log"
+	"github.com/gorilla/mux"
 )
 
 func main() {
     fmt.Println("Golang Clean Architecture")
+
+	router := mux.NewRouter()
+    router.HandleFunc("/", index).Methods("GET")
+    fmt.Println("Connected to port 9090")
+    log.Fatal(http.ListenAndServe(":9090", router))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Hallo World!"))
 }
