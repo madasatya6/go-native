540a12f0376a40d5f6387e90885a01e0681860df
Mada Satya Bayu Ambika <madasatya6@gmail.com>
2021-10-10 11:07:44 +0700

    update

--- go.mod
+++ go.mod
@@ -10,5 +10,8 @@ require (
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
+	github.com/go-kit/kit v0.12.0 // indirect
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 )
--- system/logs/logs.go
+++ system/logs/logs.go
@@ -3,13 +3,29 @@ package logs
 import (
 	"net/http"
 	"log"
+	"os"
+
 	"github.com/gorilla/mux"
+	clog "github.com/madasatya6/go-native/applications/middleware/logger"
+	klog "github.com/go-kit/kit/log"
 )
 
 type Log struct{}
 
 func (t *Log) SetRouter(router *mux.Router) {
-	router.Use(LoggingMiddleware)
+	var logger klog.Logger
+	// Logfmt is a structured, key=val logging format that is easy to read and parse
+	logger = klog.NewLogfmtLogger(klog.NewSyncWriter(os.Stderr))
+	// Direct any attempts to use Go's log package to our structured logger
+	log.SetOutput(klog.NewStdlibAdapter(logger))
+	// Log the timestamp (in UTC) and the callsite (file + line number) of the logging
+	// call for debugging in the future.
+	logger = klog.With(logger, "ts", klog.DefaultTimestampUTC, "loc", klog.DefaultCaller)
+	// Create an instance of our LoggingMiddleware with our configured logger
+	loggingMiddleware := clog.LoggingMiddleware(logger)
+	loggingMiddleware(router)
+		
+	router.Use(loggingMiddleware)
 }
 
 func LoggingMiddleware(next http.Handler) http.Handler {
