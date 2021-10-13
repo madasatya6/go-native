4b1ce365619e243b4aa0d19d97930f1805c2fab6
Mada Satya Bayu Ambika <madasatya6@gmail.com>
2021-10-13 07:13:29 +0700

    update

---
+++ applications/config/global.go
@@ -0,0 +1,6 @@
+package config
+
+import "database/sql"
+
+var MySQL *sql.DB
+var postgree *sql.DB
---
+++ system/database/database.go
@@ -0,0 +1,88 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+)
+
+var DB Databases
+
+var MySQLDsn = fmt.Sprintf("%v:%v@/%v", "root", "", "go-ecommerce")
+var MySQLDockerDsn = fmt.Sprintf("%v:%v@tcp(%s)/%v", "root", "lampupijar77", "mysql-network", "go-ecommerce") //docker
+var postgreeDsn = "dbname=ecommerce user=postgres password=lampupijar77 host=localhost sslmode=disable"
+
+type Methods interface{
+	SetMysql(dsn string)
+	SetPostgre(dsn string)
+	TestPing(dbNames []string)
+}
+
+type Databases struct{
+	MySQL *sql.DB
+	Postgre *sql.DB
+}
+
+func (d *Databases) SetMysql(dsn string) {
+	db, _ := MySQL(dsn)
+	d.MySQL = db 
+}
+
+func (d *Databases) SetPostgre(dsn string) {
+	db, _ := Postgres(dsn)
+	d.Postgre = db 
+}
+
+func (d *Databases) TestPing(dbNames []string) {
+	var err Error
+	for i:=0; i < len(dbNames); i++ {
+		if dbNames[i] == "mysql" {
+			err = d.MySQL.Ping()
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		} else if dbNames[i] == "postgree" {
+			err = d.Postgre.Ping()
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		}
+	}
+
+	if err == nil {
+		log.Println("Successfully connected with database")
+	}
+}
+
+func Init() *Databases {
+	var method Methods
+	method = &DB 
+	method.SetMysql()
+	method.SetPostgre()
+	method.TestPing([]string{
+		'mysql'
+	})
+	return &DB
+}
+
+func MySQL(config string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", config)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func Postgres(config string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", config)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
