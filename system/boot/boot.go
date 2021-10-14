acc74836f756bab97a2f743f50d219ff3c4a335c
Mada Satya Bayu Ambika <madasatya6@gmail.com>
2021-10-14 10:28:59 +0700

    update

--- system/boot/boot.go
+++ system/boot/boot.go
@@ -3,11 +3,13 @@ package boot
 import (
 	"net/http"
 	"github.com/madasatya6/go-native/applications/config"
+	"github.com/madasatya6/go-native/system/times"
 )
 
 func Init(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
 		go config.RegisterBoots()
+		go times.Init()
 		next.ServeHTTP(w,r)
 	})
 }
