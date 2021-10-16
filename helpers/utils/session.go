b1d8578c3ca77240c3aa3fe9794382d1a9ba6ca6
Mada Satya Bayu Ambika <madasatya6@gmail.com>
2021-10-16 08:04:39 +0700

    update

--- helpers/utils/session.go
+++ helpers/utils/session.go
@@ -16,24 +16,20 @@ var SessionCookie *sessions.CookieStore
 var SessionMySQL *mysqlstore.MySQLStore
 var SessionPostgre *pgstore.PGStore
 
-//set type session
-var SessionStore = newCookieStore()
-
 func Session(r *http.Request) (*sessions.Session, error) {
-	//SessionCookie = newCookieStore()
 	sess, err := SessionCookie.Get(r, config.SessionID)
 	return sess, err
 }
 
 func SetFlashdata(w http.ResponseWriter, r *http.Request, name, value string){
-	session, _ := SessionStore.Get(r, "fmessages")
+	session, _ := SessionCookie.Get(r, "fmessages")
 	session.AddFlash(value, name)
 
 	session.Save(r, w)
 }
 
 func GetFlashdata(w http.ResponseWriter, r *http.Request, name string) []string {
-	session, _ := SessionStore.Get(r, "fmessages")
+	session, _ := SessionCookie.Get(r, "fmessages")
 	fm := session.Flashes(name)
 	//IF we have some message
 
@@ -64,17 +60,6 @@ func SetPostgre(sess *pgstore.PGStore){
 	SessionPostgre = sess
 }
 
-func newCookieStore() *sessions.CookieStore {
-	authKey := []byte("my-auth-key-very-secret")
-	encryptionKey := []byte("my-encryption-key-very-secret123")
-
-	store := sessions.NewCookieStore(authKey,encryptionKey)
-	store.Options.Path = "/"
-	store.Options.MaxAge = 86400 * 7 //expired dalam seminggu
-	store.Options.HttpOnly = true
-
-	return store
-}
 
 
 
