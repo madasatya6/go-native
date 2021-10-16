4297b48cf6dd981a7ed98c7554bdd7f7b8551520
Mada Satya Bayu Ambika <madasatya6@gmail.com>
2021-10-16 08:36:21 +0700

    update

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"log"
     "sync"
     "net/http"
+    "crypto/tls"
 
 	"github.com/madasatya6/go-native/system"
 )
 
 /**
-* CLEAN ARCHITECTURE GOLANG
+* GO CLEAN ARCHITECTURE
 * 
 * @author Mada Satya Bayu Ambika
 * @version 1.13
@@ -31,6 +32,9 @@ func main() {
     systems.LogEntry.SetRouter(router)
     fmt.Println("Connected to port ", env.Port)
 
+    //advanced server
+	//server.TLSConfig = getTlsConfig() //uncomment to activate advanced server
+
     lock := make(chan error)
     srv := &http.Server{
         Handler:      router, //type http.Handler
@@ -48,5 +52,35 @@ func main() {
     wg.Wait()
 
     log.Fatal(lock)
+}
+
+func getTlsConfig() *tls.Config {
+	certPair1, err := tls.LoadX509KeyPair("server.crt", "server.key")
+	if err != nil {
+		log.Fatalln("Failed to start web server", err)
+	}
+
+	tlsConfig := new(tls.Config)
+	tlsConfig.NextProtos = []string{"http/1.1"}
+	tlsConfig.MinVersion = tls.VersionTLS12
+	tlsConfig.PreferServerCipherSuites = true
+
+	tlsConfig.Certificates = []tls.Certificate{
+		certPair1, /** add other certificates here **/
+	}
+	tlsConfig.BuildNameToCertificate()
+
+	tlsConfig.ClientAuth = tls.VerifyClientCertIfGiven
+	tlsConfig.CurvePreferences = []tls.CurveID{
+		tls.CurveP521,
+		tls.CurveP384,
+		tls.CurveP256,
+	}
+	tlsConfig.CipherSuites = []uint16{
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+	}
+
+	return tlsConfig
 }
 
